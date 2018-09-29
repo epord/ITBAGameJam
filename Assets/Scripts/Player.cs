e72df05e482de06d0b57d18166839c3554c3f33b
HubertHamelin <32563790+HubertHamelin@users.noreply.github.com>
2018-09-29 12:34:07 -0300

    First moving tests

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float jumpForce;
+    public int normalSpeed;
+    public int inJumpSpeed;
+    public int currentSpeed;
+    private Rigidbody m_rigidbody;
+    public bool isJumping;
+
+    private void Start()
+    {
+        m_rigidbody = GetComponent<Rigidbody>();
+        isJumping = false;
+        currentSpeed = normalSpeed;
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector2.left * currentSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector2.right * currentSpeed * Time.deltaTime);
+        }
+        else
+        {
+            // Default ?
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
+        {
+            m_rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isJumping = true;
+            currentSpeed = inJumpSpeed;
+        }        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "MapElement" && isJumping)
+        {
+            currentSpeed = normalSpeed;
+            isJumping = false;
+        }
+    }
+}
