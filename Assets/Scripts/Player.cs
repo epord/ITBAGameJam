abb6bbab467ef083219c61e6bfc0663a07ebb275
Tomas Raies <traies@itba.edu.ar>
2018-09-29 18:22:47 -0300

    Fix movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,31 +4,28 @@ using UnityEngine;
 
 public class Player : MonoBehaviour, ILevelEntity
 {
+    public float JumpDistance = 0.1f;
     private Vector3 _spawnPosition;
     public float jumpForce = 15.0f;
-    public int normalSpeed = 5;
-    public int inJumpSpeed = 4;
-    public int currentSpeed = 5;
+    public float normalSpeed = 5;
+    public float inJumpSpeed = 4;
+    public float currentSpeed = 5;
     public GameObject mesh;
     public int TotalLives;
     private int _lives;
     private Rigidbody m_rigidbody;
-    public bool isJumping;
     private Vector2 currentDir;
-    private int collisionCounter;
 
     private void Start()
     {
         m_rigidbody = GetComponent<Rigidbody>();
-        isJumping = false;
         currentSpeed = normalSpeed;
         _spawnPosition = transform.position;
         _lives = TotalLives;
-        collisionCounter = 0;
         mesh.transform.eulerAngles = new Vector3(-90.0f, 0.0f, 180.0f);
     }
 
-    void FixedUpdate()
+    void Update()
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
@@ -44,51 +41,32 @@ public class Player : MonoBehaviour, ILevelEntity
         {
             currentDir = Vector2.up;
         }
-
-        if (!isJumping && !Input.anyKey)
+        else
         {
             currentDir = Vector2.zero;
         }
 
-        if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            Vector3 dir = new Vector3(currentDir.x, currentDir.y, 0);
-            m_rigidbody.AddForce((Vector3.up * jumpForce) + (dir * inJumpSpeed), ForceMode.Impulse);
-            isJumping = true;
-            currentSpeed = inJumpSpeed;
+            if (Physics.Raycast(transform.position, Vector3.down, JumpDistance)) {
+                Vector3 dir = new Vector3(currentDir.x, currentDir.y, 0);
+                m_rigidbody.AddForce((Vector3.up * jumpForce) + (dir * inJumpSpeed), ForceMode.Impulse);
+                currentSpeed = inJumpSpeed * 1.5f;
+            }
         }
-        var pos = new Vector2(m_rigidbody.position.x, m_rigidbody.position.y);
-        m_rigidbody.position = pos + currentDir * currentSpeed * Time.fixedDeltaTime;
-        
     }
 
-    private void OnCollisionEnter(Collision collision)
+    void FixedUpdate()
     {
-        if (collision.gameObject.tag == "MapElement" && isJumping /*&& collision.contacts[0].point.y < transform.position.y*/)
-        {
-            currentSpeed = normalSpeed;
-            isJumping = false;
-            collisionCounter++;
-        }
+        var pos = new Vector2(m_rigidbody.position.x, m_rigidbody.position.y);
+        m_rigidbody.position = pos + currentDir * currentSpeed * Time.fixedDeltaTime;
     }
+    
     
     public void Reset()
     {
         transform.position = _spawnPosition;
         m_rigidbody.velocity = new Vector3();
-    }
-
-    private void OnCollisionExit(Collision collision)
-    {
-        if (collision.gameObject.tag == "MapElement")
-        {
-            collisionCounter--;
-            if(collisionCounter == 0)
-            {
-                currentSpeed = inJumpSpeed;
-                isJumping = true;
-            }
-        }
     }
     
     public int GetHp()
